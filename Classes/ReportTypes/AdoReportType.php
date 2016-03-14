4e14f1d6ee05bcf662920221cccdc9f9a34496e2
Todd Eidson <teidson@bapm.com>
2016-03-14 11:46:41 -0400

    Added 'use statement' for PhpReports Class

--- Classes/ReportTypes/AdoReportType.php
+++ Classes/ReportTypes/AdoReportType.php
@@ -5,6 +5,7 @@ namespace Eidsonator\BootstrapReportsBundle\Classes\ReportTypes;
 use Eidsonator\BootstrapReportsBundle\lib\PhpReports\ReportTypeBase;
 use Eidsonator\BootstrapReportsBundle\lib\PhpReports\Report;
 use Eidsonator\BootstrapReportsBundle\lib\SqlFormatter\lib\SqlFormatter;
+Use Eidsonator\BootstrapReportsBundle\lib\PhpReports\PhpReports;
 
 class AdoReportType extends ReportTypeBase
 {
